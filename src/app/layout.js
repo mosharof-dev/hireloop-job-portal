aa57ab4334659da4dd9477c925504e4828c30dc1
mosharof-dev <mosharof.dev@gmail.com>
2026-05-25 18:44:11 +0600

    feat: implement responsive Navbar and integrate it into the root layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/shared/NavBer";
+import Navbar from "@/components/shared/NavBar";
 
 
 const geistSans = Geist({
