7638230dc1b38ec82c0391d25fce2dd7aa97d5ff
mosharof-dev <mosharof.dev@gmail.com>
2026-06-07 18:36:38 +0600

    feat: implement recruiter job posting page and form with company data fetching

--- src/lib/api/companies.js
+++ src/lib/api/companies.js
@@ -1,7 +1,15 @@
 import { serverGet } from "../core/server";
-
+import { getUserSession } from "../core/session";
 
 // get company profile function
 export const getCompanyProfile = async (recruiterId) => {
-  return serverGet(`/api/my/companies?recruiterId=${recruiterId}`);
+  const companies = await serverGet(
+    `/api/my/companies?recruiterId=${recruiterId}`,
+  );
+  return companies.length > 0 ? companies[0] : null;
+};
+
+export const getLoggedInRecruiterCompany = async () => {
+  const user = await getUserSession();
+  return  getCompanyProfile(user?.id);
 };
