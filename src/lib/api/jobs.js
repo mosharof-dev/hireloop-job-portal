de6f27b865310289a7f2af44c1af8f96da828210
mosharof-dev <mosharof.dev@gmail.com>
2026-06-13 23:48:44 +0600

    style: format code for consistency in jobs and plans API files-

--- src/lib/api/jobs.js
+++ src/lib/api/jobs.js
@@ -1,22 +1,23 @@
 import { serverGet } from "../core/server";
 
- const API_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:5000'
+const API_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:5000";
 
- export const getAllJobs = async ()=>{
-    return serverGet(`/api/jobs`)
- }
-export const getJobById = async (jobId)=>{
-    return serverGet(`/api/jobs/${jobId}`)
-}
+export const getAllJobs = async () => {
+  return serverGet(`/api/jobs`);
+};
+export const getJobById = async (jobId) => {
+  return serverGet(`/api/jobs/${jobId}`);
+};
 // get company all jobs function
-export const getCompanyJobs = async (companyId, status= 'active') => {
-    try {
-        const res = await fetch(`${API_URL}/api/jobs?companyId=${companyId}&status=${status}`);
-        const data = await res.json();
-        return data;
-    }
-    catch (error) {
-        console.error('Error fetching company jobs:', error);
-        throw error;
-    }
+export const getCompanyJobs = async (companyId, status = "active") => {
+  try {
+    const res = await fetch(
+      `${API_URL}/api/jobs?companyId=${companyId}&status=${status}`,
+    );
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching company jobs:", error);
+    throw error;
+  }
 };
--- src/lib/api/plans.js
+++ src/lib/api/plans.js
@@ -1,6 +1,5 @@
-import { serverGet } from "../core/server"
+import { serverGet } from "../core/server";
 
-
-export const getPlanById = async (plan_Id)=>{
-    return serverGet(`/api/plans?planId=${plan_Id}`)
-}
+export const getPlanById = async (plan_Id) => {
+  return serverGet(`/api/plans?planId=${plan_Id}`);
+};
